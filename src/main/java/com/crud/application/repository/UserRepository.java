69c2baed388d93761d49649e92be11c5e6b0d9fd
Vijay balaji <balajibaskaran456@gmail.com>
2026-07-14 12:24:21 +0530

    refactor: Update pan email logic changed

--- src/main/java/com/crud/application/repository/UserRepository.java
+++ src/main/java/com/crud/application/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.crud.application.repository;
 
 import com.crud.application.entity.UserEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,7 +14,11 @@ public interface UserRepository extends JpaRepository<UserEntity,Integer> {
     UserEntity findByName(String name);
     Optional<UserEntity> findByUsername(String username);
 
-    //JPQL QUERY for fetch user along with Pan details
-    @Query(value = "SELECT u FROM UserEntity u JOIN FETCH u.panId WHERE u.userId=:userId")
-    UserEntity fetchUserWithPan(@Param("userId") int userId);
+//    //JPQL QUERY for fetch user along with Pan details
+//    @Query(value = "SELECT u FROM UserEntity u JOIN FETCH u.panId WHERE u.userId=:userId")
+//    UserEntity fetchUserWithPan(@Param("userId") int userId);
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE PanEntity p SET p.email=:email WHERE p.userId.userId=:userId")
+    int updatePanEmail(@Param("userId")int userId,@Param("email")String email);
 }
